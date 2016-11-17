4129be1a58c2bccf2b7d447de9e632342c0da07e
niereo <abdelaliqochih@yahoo.it>
2016-11-17 16:01:50 +0100

    Update Animale.java

--- src/Animale.java
+++ src/Animale.java
@@ -11,9 +11,11 @@
 public class Animale {
     private final String nome;
     private final String specie;
+    private final Boolean malato;
     public Animale(String n,String s ){
         this.nome = n;
         this.specie = s;
+        this.malato = false;
         System.out.println("ho aggiunto un " + s + " di nome " + n );
         
     }
